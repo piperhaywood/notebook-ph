c29b6adec45008df30461a53a9418aabd2c63f4b
Piper Haywood <piper@sbphstudio.com>
2016-06-29 13:50:13 +0100

    Address default font stack
    Fixes #13

--- wp-content/themes/notebook-ph/library/enqueue.php
+++ wp-content/themes/notebook-ph/library/enqueue.php
@@ -4,6 +4,15 @@ add_action( 'wp_enqueue_scripts', 'nph_assets' );
 function nph_assets() {
   $version = nph_get_theme_version();
 
+  wp_register_style(
+    'nph-styles',
+    get_template_directory_uri() . '/style.css',
+    '',
+    $version,
+    'all'
+  );
+  wp_enqueue_style( 'nph-styles' );
+
   if ( file_exists( get_template_directory() . '/fonts.css' ) ) {
     wp_register_style(
       'nph-fonts',
@@ -14,15 +23,6 @@ function nph_assets() {
     );
     wp_enqueue_style( 'nph-fonts');
   }
-
-  wp_register_style(
-    'nph-styles',
-    get_template_directory_uri() . '/style.css',
-    '',
-    $version,
-    'all'
-  );
-  wp_enqueue_style( 'nph-styles' );
 
   // wp_register_script(
   //   'modernizr',

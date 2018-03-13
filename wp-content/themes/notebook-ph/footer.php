64aa80860bc048d857cab09b59f0470faa6ae9b7
Piper Haywood <piper.haywood@gmail.com>
2018-03-13 16:01:38 +0000

    Clean up pagination

--- wp-content/themes/notebook-ph/footer.php
+++ wp-content/themes/notebook-ph/footer.php
@@ -28,8 +28,8 @@ if ($total) {
         <?php $prev = get_previous_posts_link(__('Previous page', 'notebook-ph')); ?>
         <?php $next = get_next_posts_link(__('Next page', 'notebook-ph')); ?>
       <?php elseif (is_singular('post')) : ?>
-        <?php $prev = get_previous_post_link('%link', __('Previous ', 'notebook-ph') . get_posts_label(false)); ?>
-        <?php $next = get_next_post_link('%link', __('Next ', 'notebook-ph') . get_posts_label(false)); ?>
+        <?php $prev = get_previous_post_link('%link', __('Previous', 'notebook-ph')); ?>
+        <?php $next = get_next_post_link('%link', __('Next', 'notebook-ph')); ?>
       <?php endif; ?>
       <?php if ($prev || $next) : ?>
         <nav class="pagination">

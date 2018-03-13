3f47996be9309328869e09d9c19c9fdf34ca480a
Piper Haywood <piper.haywood@gmail.com>
2018-03-13 12:45:43 +0000

    Add wrapper t 404

--- wp-content/themes/notebook-ph/404.php
+++ wp-content/themes/notebook-ph/404.php
@@ -4,10 +4,12 @@
 
 <main class="content <?php nph_pagetype(); ?>">
   <article class="post gradient type-<?php echo get_post_type(); ?>"<?php if ($hsl) : ?> style="--first-color:<?php echo $hsl; ?>;--second-color: var(--first-color);"<?php endif; ?>>
-    <div class="container">
-      <div class="post__content">
-        <p>Sorry, there&rsquo;s nothing to see here. Please browse the content using the tools below, or get in touch if you can&rsquo;t find what you&rsquo;re looking for.</p>
-        <?php get_template_part('content', 'browse'); ?>
+    <div class="wrapper">
+      <div class="container">
+        <div class="post__content">
+          <p>Sorry, there&rsquo;s nothing to see here. Please browse the content using the tools below, or get in touch if you can&rsquo;t find what you&rsquo;re looking for.</p>
+          <?php get_template_part('content', 'browse'); ?>
+        </div>
       </div>
     </div>
   </article>

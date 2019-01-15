78422e5047541fb39e81add15855c22e9658854e
Piper Haywood <piper.haywood@gmail.com>
2019-01-15 18:15:38 +0000

    Add infinite scroll

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,23 @@
   $('select').hover(function() {
     $(this).css('cursor', 'pointer');
   });
+  $next = $('.js-next');
+  if ($next.length) {
+    var $container = $('.js-infinite-container');
+    $container.infiniteScroll({
+      path: '.js-next a',
+      append: '.js-article',
+      hideNav: '.js-pagination',
+      elementScroll: '.js-wrapper'
+    });
+    $container.on('request.infiniteScroll', function(event, path) {
+      $('.js-infinite-loading').addClass('show');
+    });
+    $container.on('last.infiniteScroll', function(event, response, path) {
+      $('.js-infinite-loading').removeClass('show');
+      $('.js-infinite-end').addClass('show');
+    });
+  }
 
   $('.post').fitVids();
 

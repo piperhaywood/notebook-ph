15b485076b506427051bdd322ed7eb36b94f8ec3
Piper Haywood <piper.haywood@gmail.com>
2019-06-20 10:21:15 +0100

    Prettier formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,46 @@
 (function($) {
-
-  $next = $('.js-next');
+  $next = $(".js-next");
   if ($next.length) {
-    var $container = $('.js-infinite-container');
+    var $container = $(".js-infinite-container");
     $container.infiniteScroll({
-      path: '.js-next a',
-      append: '.js-article',
-      hideNav: '.js-pagination',
-      elementScroll: '.js-wrapper'
+      path: ".js-next a",
+      append: ".js-article",
+      hideNav: ".js-pagination",
+      elementScroll: ".js-wrapper"
     });
-    $container.on('append.infiniteScroll', function(event, response, path, items) {
-      $(items).find('img').each(function(index, img) {
-        img.outerHTML = img.outerHTML;
-      });
+    $container.on("append.infiniteScroll", function(
+      event,
+      response,
+      path,
+      items
+    ) {
+      $(items)
+        .find("img")
+        .each(function(index, img) {
+          img.outerHTML = img.outerHTML;
+        });
       // Reset audio HTML due to Infinite Scroll behaviour
-      $(items).find('audio').each(function(index, audio) {
-        audio.outerHTML = audio.outerHTML;
-      });
+      $(items)
+        .find("audio")
+        .each(function(index, audio) {
+          audio.outerHTML = audio.outerHTML;
+        });
     });
-    $container.on('request.infiniteScroll', function(event, path) {
-      $('.js-infinite-loading').addClass('show');
+    $container.on("request.infiniteScroll", function(event, path) {
+      $(".js-infinite-loading").addClass("show");
     });
-    $container.on('last.infiniteScroll', function(event, response, path) {
-      $('.js-infinite-loading').removeClass('show');
-      $('.js-infinite-end').addClass('show');
+    $container.on("last.infiniteScroll", function(event, response, path) {
+      $(".js-infinite-loading").removeClass("show");
+      $(".js-infinite-end").addClass("show");
     });
   }
 
   function handleFirstTab(e) {
-    if (e.keyCode === 9) { // the "I am a keyboard user" key
-      document.body.classList.add('user-is-tabbing');
-      window.removeEventListener('keydown', handleFirstTab);
+    if (e.keyCode === 9) {
+      // the "I am a keyboard user" key
+      document.body.classList.add("user-is-tabbing");
+      window.removeEventListener("keydown", handleFirstTab);
     }
   }
-  window.addEventListener('keydown', handleFirstTab);
-
+  window.addEventListener("keydown", handleFirstTab);
 })(jQuery);

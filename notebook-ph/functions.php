85b743690cfbb1fb847b28d0ee440bb610497f5f
Piper Haywood <piper.haywood@gmail.com>
2019-10-03 18:42:41 +0100

    Improve comments markup
    See #30

--- notebook-ph/functions.php
+++ notebook-ph/functions.php
@@ -544,3 +544,60 @@ function nph_get_list($posts = false) {
     <?php return ob_get_clean();
   }
 }
+
+function nph_comment($comment, $args, $depth) {
+  if ( 'div' === $args['style'] ) {
+    $tag       = 'div';
+    $add_below = 'comment';
+  } else {
+    $tag       = 'li';
+    $add_below = 'div-comment';
+  }?>
+  <<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID() ?>"><?php 
+  if ( 'div' != $args['style'] ) { ?>
+    <div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
+  } ?>
+    <div class="comment-avatar">
+      <?php
+        if ( $args['avatar_size'] != 0 ) {
+          echo get_avatar( $comment, $args['avatar_size'] ); 
+        }
+      ?>
+    </div>
+    <div class="comment-author vcard">
+        <?php printf( __( '<cite class="fn">%s</cite>' ), get_comment_author_link() ); ?>, 
+      <span class="comment-meta commentmetadata">
+        <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>"><?php
+          /* translators: 1: date, 2: time */
+          printf( 
+            __('%1$s at %2$s'),
+            get_comment_date(),
+            get_comment_time()
+          ); ?>
+        </a>
+      </span><?php edit_comment_link( __( 'Edit' ), ' / ', '' ); ?>
+    </div>
+
+    <div class="comment-content prose">
+      <?php if ( $comment->comment_approved == '0' ) : ?>
+        <p><em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em></p>
+      <?php endif; ?>
+      <?php comment_text(); ?>
+    </div>
+
+    <div class="reply"><?php
+      comment_reply_link(
+        array_merge(
+          $args,
+          array(
+            'add_below' => $add_below,
+            'depth'     => $depth,
+            'max_depth' => $args['max_depth']
+          )
+        )
+      ); ?>
+    </div><?php
+  if ( 'div' != $args['style'] ) : ?>
+    </div><?php
+  endif;
+}
